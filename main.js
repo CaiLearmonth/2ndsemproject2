91692b2c20eab19a9f526b7d8c421eca048ad034
CaiLearmonth <Learmonthhcai@gmail.com>
2025-02-23 23:46:58 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+//function changecolor(){
+
+
+
+//const colortext = document.getElementById("text1")
+//colortext.innerText = "Get ELement by id (GEBI) ";
+//colortext.style.backgroundColor = "blue";
+//colortext.style.padding = "90px";
+
+//const baybay = document.getElementById("baybay");
+//baybay.style.fontSize= "20px"
+
+
+
+
+//document.body.style.backgroundColor = "violet"
+//}
+
+
+//function Changecolor(){
+  //  const colortext = document.getElementById("text1");
+    //colortext.innertext = "hellorsss";
+   // colortext.style.color = "red";
+   // colortext.style.backgroundColor = "black";
+   // colortext.style.padding = "90px";
+   //document.body.style.backgroundColor = "yellow";
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+function clickMe(){
+  const input = document.getElementById("input").value;
+  console.log(input)
+  document.body.style.backgroundColor = input;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
